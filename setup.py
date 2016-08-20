4aaf47434d8e71aad0aff96fb3cc1a8f087be841
Gerard Keating (mac) <gerardk@gmail.com>
2016-08-20 15:40:30 +0100

    Getting setup to work

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+# based on https://github.com/pypa/sampleproject/blob/master/setup.py
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
 
 version = '0.0.1'
 
@@ -12,13 +16,13 @@ setup(
     long_description = long_description,
     author = 'Gerard Keating',
     author_email = 'gerardk@gmail.com',
-    license = 'LICENCE.txt',
+    license = 'MIT',
     url = 'https://github.com/vfxGer/pretty-numbers',
     py_modules = ['pretty_numbers'],
     download_url = 'https://pypi.python.org/packages/source/d/pretty_numbers-%s.tar.gz?raw=true' % (version),
     platforms='Cross-platform',
     classifiers=[
-      'Programming Language :: Python',
+      'Programming Language :: Python :: 2',
       'Programming Language :: Python :: 3'
     ],
 )
