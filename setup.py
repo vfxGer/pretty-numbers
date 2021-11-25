e236675493f627dd76fd0f544275484dd5af2af2
Gerard Keating <g.keating@keelvar.com>
2021-11-25 16:14:29 +0000

    Reformating setup.py

--- setup.py
+++ setup.py
@@ -4,25 +4,24 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-version = '0.0.8'
+version = "0.0.8"
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst')) as f:
+with open(path.join(here, "README.rst")) as f:
     long_description = f.read()
 
 setup(
-    name = 'prettynumbers',
-    version = version,
-    description = 'Display a range of numbers in a human readable way',
-    long_description = long_description,
-    author = 'Gerard Keating',
-    author_email = 'gerardk@gmail.com',
-    license = 'GNU GENERAL PUBLIC LICENSE',
-    url = 'https://github.com/vfxGer/pretty-numbers',
-    py_modules = ['pretty_numbers'],
-    download_url = 'https://pypi.python.org/packages/source/d/pretty_numbers-%s.tar.gz?raw=true' % (version),
-    platforms='Cross-platform',
-    classifiers=[
-      'Programming Language :: Python :: 3'
-    ],
+    name="prettynumbers",
+    version=version,
+    description="Display a range of numbers in a human readable way",
+    long_description=long_description,
+    author="Gerard Keating",
+    author_email="gerardk@gmail.com",
+    license="GNU GENERAL PUBLIC LICENSE",
+    url="https://github.com/vfxGer/pretty-numbers",
+    py_modules=["pretty_numbers"],
+    download_url="https://pypi.python.org/packages/source/d/pretty_numbers-%s.tar.gz?raw=true"
+    % (version),
+    platforms="Cross-platform",
+    classifiers=["Programming Language :: Python :: 3"],
 )
