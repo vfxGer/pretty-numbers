43d87cf9a75d238a753a2d41f94365ecd7e85801
Gerard Keating <g.keating@keelvar.com>
2021-11-26 11:55:27 +0000

    adds twine and test publish to circle ci

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # based on https://github.com/pypa/sampleproject/blob/master/setup.py
-from setuptools import setup, find_packages
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-version = "0.0.8"
+version = "0.1.0"
 
 # Get the long description from the README file
 with open(path.join(here, "README.rst")) as f:
@@ -15,6 +15,7 @@ setup(
     version=version,
     description="Display a range of numbers in a human readable way",
     long_description=long_description,
+    long_description_content_type="text/x-rst",
     author="Gerard Keating",
     author_email="gerardk@gmail.com",
     license="GNU GENERAL PUBLIC LICENSE",
